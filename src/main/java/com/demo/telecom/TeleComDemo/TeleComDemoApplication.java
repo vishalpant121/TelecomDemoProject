8350fc2950d63bcec1f8f70d3b6791095d4416c9
VISPANT <VISPANT@VISPANT-PORTEGE.in.oracle.com>
2022-09-11 22:50:22 +0530

    first commit

---
+++ src/main/java/com/demo/telecom/TeleComDemo/TeleComDemoApplication.java
@@ -0,0 +1,13 @@
+package com.demo.telecom.TeleComDemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeleComDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TeleComDemoApplication.class, args);
+	}
+
+}
